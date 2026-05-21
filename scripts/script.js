805f3a6bc8f9147d0594de2ca0248a433c791d46
ElisaTorres24 <elisad.torres18@gmail.com>
2026-05-21 11:21:19 -0300

    começando Js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//ativando o menu mobile
+
+function mostraMenu() {
+    $('#nav-esq ul#menu-principal').css('display', 'flex');
+    $('#nav-esq ul#menu-principal').addClass('animate__animated animate__fadeInRight animate__slow');
+    
+}
