46c978947e22eb4fa511a8b8ea2f682763cade7e
ElisaTorres24 <elisad.torres18@gmail.com>
2026-05-21 13:25:04 -0300

    menu mobile pronto

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,26 @@
 function mostraMenu() {
     $('#nav-esq ul#menu-principal').css('display', 'flex');
     $('#nav-esq ul#menu-principal').addClass('animate__animated animate__fadeInRight animate__slow');
-    
+
+    $('#nav-esq ul#icone-menu li#menu').css('display', 'none');
+    $('#nav-esq ul#icone-menu li#menuX').css('display', 'flex');   
 }
+
+function esconderMenu() {
+    $('#nav-esq ul#menu-principal').css('display', 'none');
+
+    $('#nav-esq ul#icone-menu li#menu').css('display', 'flex');
+    $('#nav-esq ul#icone-menu li#menuX').css('display', 'none');
+}
+
+let controle = true;
+
+$('#nav-esq ul#icone-menu li#menu').click(function() {
+    if (controle == true) {
+        mostraMenu();
+        controle = false;
+    } else {
+        esconderMenu();
+        controle = true;
+    }
+});
